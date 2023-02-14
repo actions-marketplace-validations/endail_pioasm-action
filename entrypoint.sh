3491ed770dcc8d3bbee1e44bbe077312d7197093
endail <52652357+endail@users.noreply.github.com>
2023-02-14 21:00:03 +1100

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
-#apt update
-#apt install -y git make cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential
+git clone https://github.com/raspberrypi/pico-sdk.git --branch master
+cd pico-sdk
+git submodule update --init
+export PICO_SDK_PATH="$(pwd)"
+cd ..
 
-#git clone https://github.com/raspberrypi/pico-sdk.git --branch master
-#cd pico-sdk
-#git submodule update --init
+mkdir pioasm_build
+cd pioasm_build
+cmake "$PICO_SDK_PATH/tools/pioasm"
+make
+cd ..
 
-#export PICO_SDK_PATH="$(pwd)"
-#cd ..
-
-#mkdir pioasm_build
-#cd pioasm_build
-#cmake $PICO_SDK_PATH/tools/pioasm
-#make
-#cd ..
-
-ls -al
-ls -al ..
-
-/pioasm_build/pioasm -o "$1" "$2" >> out.pio.h
+pioasm_build/pioasm -o "$1" "$2" >> out.pio.h
 
 echo "out<<EOF" >> $GITHUB_OUTPUT
 echo "$(cat out.pio.h)" >> $GITHUB_OUTPUT
