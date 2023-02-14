893802810b3501d2bb0b94dd3d0a8f0624353936
endail <52652357+endail@users.noreply.github.com>
2023-02-14 17:01:01 +1100

    custom image

---
+++ image/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt update
+apt install -y git make cmake gcc-arm-none-eabi libnewlib-arm-none-eabi build-essential
+
+git clone https://github.com/raspberrypi/pico-sdk.git --branch master
+cd pico-sdk
+git submodule update --init
+
+export PICO_SDK_PATH="$(pwd)"
+cd ..
+
+mkdir pioasm_build
+cd pioasm_build
+cmake $PICO_SDK_PATH/tools/pioasm
+make
+cd ..
