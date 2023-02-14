cb950b6e189d3aa9ce013da135e5cb5d1ec0cadc
endail <52652357+endail@users.noreply.github.com>
2023-02-14 22:12:45 +1100

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,39 @@
 #!/bin/bash
 
+# get the pico sdk
 git clone https://github.com/raspberrypi/pico-sdk.git --branch master
 cd pico-sdk
 export PICO_SDK_PATH="$(pwd)"
 cd ..
 
+# build pioasm
 mkdir pioasm_build
 cd pioasm_build
 cmake "$PICO_SDK_PATH/tools/pioasm"
 make
 cd ..
 
-pioasm_build/pioasm -o "$1" "$2" >> out.pio.h
+# if $2 is empty, set outpath to current dir of piofile
+if [ ! -z "$2" ]; do
+    outpath="."
+else
+    outpath="$2"
+fi
 
-echo "out<<EOF" >> $GITHUB_OUTPUT
-echo "$(cat out.pio.h)" >> $GITHUB_OUTPUT
-echo "EOF" >> $GITHUB_OUTPUT
+for piofile in "$1"; do
+
+    if [ ! -f "$piofile" ]; then
+        echo "$piofile not found; skipping"
+        continue
+    fi
+
+    pioasm_build/pioasm "$piofile" >> "$outpath/$piofile.h"
+
+    ls -al $(dirname "$piofile")
+
+done
+
+# send to gha
+#echo "out<<EOF" >> $GITHUB_OUTPUT
+#echo "$(cat out.pio.h)" >> $GITHUB_OUTPUT
+#echo "EOF" >> $GITHUB_OUTPUT
